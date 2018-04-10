5fae079354d264109190a5783388eca59722f72e
swsong <swsong@gncloud.kr>
2018-04-10 14:08:51 +0900

    .

--- src/test/java/io/gncloud/coin/aws/AwsCognitoTest.java
+++ src/test/java/io/gncloud/coin/aws/AwsCognitoTest.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -69,7 +70,7 @@ public class AwsCognitoTest {
         AuthenticationResultType authResult = authResponse.getAuthenticationResult();
 
         logger.info("session>> {}", session);
-        logger.info("authResult>> {}, authResult");
+        logger.info("authResult>> {}", authResult);
 
 
         if(challengeName != null) {
@@ -103,10 +104,29 @@ public class AwsCognitoTest {
             accessToken = authResult.getAccessToken();
             JSONObject header = CognitoJWTParser.getHeader(accessToken);
             JSONObject payload = CognitoJWTParser.getPayload(accessToken);
+            String signature = CognitoJWTParser.getSignature(accessToken);
+            logger.debug("jwt > {}", accessToken);
             logger.debug("header > {}", header);
             logger.debug("payload > {}", payload);
+            logger.debug("signature > {}", signature);
         }
 
+
+    }
+
+
+    @Test
+    public void testJwtSignature() {
+        String jwt = "eyJraWQiOiJGcm9lZEZMNXBkaWlvMlh5emhsXC9xT3h2RmpUcWVPUzRNOTNpSFBhSjFEcz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxNThkOTQ3NC01YmNkLTQwYTItYmFmNS1jN2YwNjNhNmU5NTEiLCJldmVudF9pZCI6IjA4MjQ1Yzc2LTNjNTQtMTFlOC04YWEzLTNkYTJkMDQzYmQ4YiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1MjMzMTkyMjQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1ub3J0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1ub3J0aGVhc3QtMl84VWxWdUZGdmEiLCJleHAiOjE1MjMzMjI4MjQsImlhdCI6MTUyMzMxOTIyNCwianRpIjoiMDY4NDdhOTEtZGNjZC00NDhhLTg2Y2EtOTg3ZmVmZGJlNDMzIiwiY2xpZW50X2lkIjoiNGttODNqYnQxZDZwZzQxNXE0aWVxdDQxYjAiLCJ1c2VybmFtZSI6InNvbmdhYWw0In0.IDOOhlgkRnz05IKTKuDW5ZDhH_dwoFCawKZ0QRDbI89vpmpyFeaHdAuh4ZCzynnRveI96O1G7qmxjBVGVpmD8xNtz6XGI0R4d32sCcUFuo0xlhV58mvGiLPDz8-Z0fcScXtp2mJYL5j1Ng0BqlHyk8a9K7cVACfSd4fdK_qIfW16Z8HUIhOLDTWX9bgWJGAdr1mD2ekmIJf6KJ5a44YWkgguxvyBzz4yK4J4pmocp8OyqMp9u7LXSCPqrO-x1VK1FmBDpK5uXwPcCM9m9YeWtD_gkI8Nitt8s2XYrn2hTTeD21DYYLzGYOF9mjAfZnANdO7Oemc4n_G5OG_dC1TW4g";
+        byte[] decodedImg = Base64.getDecoder().decode(encodedImg.getBytes(StandardCharsets.UTF_8));
+
+
+        java.util.Base64.Decoder dec= java.util.Base64.getDecoder();
+        String encodedSignature = jwt.split("\\.")[2];
+        logger.debug("encodedSignature : {}", encodedSignature);
+        final byte[] sectionDecoded = dec.decode(encodedSignature.getBytes(StandardCharsets.UTF_8));
+
+        logger.debug("sectionDecoded : {}", sectionDecoded);
 
     }
 
