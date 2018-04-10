e356bbc48dc9021dd72c357f2b7139da524cc1e7
swsong <swsong@gncloud.kr>
2018-04-10 18:20:25 +0900

    로그인 및 인증방식 개발.

--- src/test/java/io/gncloud/coin/aws/AwsCognitoTest.java
+++ src/test/java/io/gncloud/coin/aws/AwsCognitoTest.java
@@ -3,15 +3,18 @@ package io.gncloud.coin.aws;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.*;
-import com.amazonaws.util.Base64;
-import com.amazonaws.util.StringUtils;
-import io.gncloud.coin.server.utils.CognitoJWTParser;
-import org.json.JSONObject;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.gson.Gson;
+import io.gncloud.coin.server.utils.RestUtils;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
-import java.nio.charset.StandardCharsets;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AwsCognitoTest {
@@ -37,7 +40,6 @@ public class AwsCognitoTest {
 
         AdminCreateUserResult authResponse = cognitoClient.adminCreateUser(authRequest);
         logger.info("authResponse>> {}", authResponse);
-
     }
 
 
@@ -99,37 +101,53 @@ public class AwsCognitoTest {
             }
         }
 
-
         if(authResult != null) {
             accessToken = authResult.getAccessToken();
-            JSONObject header = CognitoJWTParser.getHeader(accessToken);
-            JSONObject payload = CognitoJWTParser.getPayload(accessToken);
-            String signature = CognitoJWTParser.getSignature(accessToken);
-            logger.debug("jwt > {}", accessToken);
-            logger.debug("header > {}", header);
-            logger.debug("payload > {}", payload);
-            logger.debug("signature > {}", signature);
+            DecodedJWT decoded = JWT.decode(accessToken);
+            logger.debug("DecodedJWT > {}", decoded);
         }
 
 
     }
 
-
     @Test
-    public void testJwtSignature() {
+    public void testJwtSignature() throws UnsupportedEncodingException {
         String jwt = "eyJraWQiOiJGcm9lZEZMNXBkaWlvMlh5emhsXC9xT3h2RmpUcWVPUzRNOTNpSFBhSjFEcz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxNThkOTQ3NC01YmNkLTQwYTItYmFmNS1jN2YwNjNhNmU5NTEiLCJldmVudF9pZCI6IjA4MjQ1Yzc2LTNjNTQtMTFlOC04YWEzLTNkYTJkMDQzYmQ4YiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1MjMzMTkyMjQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1ub3J0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1ub3J0aGVhc3QtMl84VWxWdUZGdmEiLCJleHAiOjE1MjMzMjI4MjQsImlhdCI6MTUyMzMxOTIyNCwianRpIjoiMDY4NDdhOTEtZGNjZC00NDhhLTg2Y2EtOTg3ZmVmZGJlNDMzIiwiY2xpZW50X2lkIjoiNGttODNqYnQxZDZwZzQxNXE0aWVxdDQxYjAiLCJ1c2VybmFtZSI6InNvbmdhYWw0In0.IDOOhlgkRnz05IKTKuDW5ZDhH_dwoFCawKZ0QRDbI89vpmpyFeaHdAuh4ZCzynnRveI96O1G7qmxjBVGVpmD8xNtz6XGI0R4d32sCcUFuo0xlhV58mvGiLPDz8-Z0fcScXtp2mJYL5j1Ng0BqlHyk8a9K7cVACfSd4fdK_qIfW16Z8HUIhOLDTWX9bgWJGAdr1mD2ekmIJf6KJ5a44YWkgguxvyBzz4yK4J4pmocp8OyqMp9u7LXSCPqrO-x1VK1FmBDpK5uXwPcCM9m9YeWtD_gkI8Nitt8s2XYrn2hTTeD21DYYLzGYOF9mjAfZnANdO7Oemc4n_G5OG_dC1TW4g";
-        byte[] decodedImg = Base64.getDecoder().decode(encodedImg.getBytes(StandardCharsets.UTF_8));
-
-
-        java.util.Base64.Decoder dec= java.util.Base64.getDecoder();
-        String encodedSignature = jwt.split("\\.")[2];
-        logger.debug("encodedSignature : {}", encodedSignature);
-        final byte[] sectionDecoded = dec.decode(encodedSignature.getBytes(StandardCharsets.UTF_8));
-
-        logger.debug("sectionDecoded : {}", sectionDecoded);
-
+        DecodedJWT decoded = JWT.decode(jwt);
+        logger.debug("signature : {}", decoded.getSignature());
+        String payload = decoded.getPayload();
+        logger.debug("payload : {}", payload);
+        byte[] decodedPayload = Base64.getUrlDecoder().decode(payload.getBytes("utf-8"));
+        String payloadJson = new String(decodedPayload, "utf-8");
+        Gson gson = new Gson();
+        Map<String, String> payloadMap = gson.fromJson(payloadJson, Map.class);
+        logger.debug("payloadMap : {}", payloadMap);
     }
 
+
+    @Test
+    public void testPubKey(){
+
+        String userPoolId = "ap-northeast-2_8UlVuFFva";
+        String region = userPoolId.split("_")[0];
+        String url = String.format("https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json", region, userPoolId);
+
+        Map<String, Object> map = RestUtils.getObject(url, Map.class);
+        System.out.println(map);
+
+        List list = (List) map.get("keys");
+
+        if(list != null) {
+            Map keyMap = new HashMap<String, Map>();
+            for(Object km : list) {
+                keyMap.put(((Map)km).get("kid"), km);
+            }
+
+
+            System.out.println(keyMap);
+        }
+
+    }
 
 
 
