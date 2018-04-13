c3edc46df3ad24e2c98f031e7a2114b84821c936
joonwoo88 <jwkim@gncloud.kr>
2018-04-13 09:36:03 +0900

    .

--- src/main/java/io/gncloud/coin/server/service/ExchangeService.java
+++ src/main/java/io/gncloud/coin/server/service/ExchangeService.java
@@ -31,13 +31,6 @@ public class ExchangeService {
 
     public ExchangeKey insertExchangeKey (ExchangeKey exchangeKey) throws OperationException {
         try {
-//            Map<String, Object> params = new HashMap<>();
-//            params.put("userId", exchangeKey.getUserId());
-//            params.put("apiKey", exchangeKey.getApiKey());
-//            params.put("secretKey", exchangeKey.getSecretKey());
-//            params.put("exchangeName", exchangeKey.getExchangeName());
-//            params.put("name", exchangeKey.getName());
-
             int resultCount = sqlSession.insert("exchange.insertKey", exchangeKey);
             if (resultCount != 1) {
                 throw new OperationException("[FAIL] insert exchangeKey result 0");
