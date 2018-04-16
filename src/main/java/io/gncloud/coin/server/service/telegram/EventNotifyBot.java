741bfc026dcff2a24e6776c01d9e7c48e6093795
swsong <swsong@gncloud.kr>
2018-04-16 18:00:43 +0900

    텔레그램 메시징.

--- src/main/java/io/gncloud/coin/server/service/telegram/EventNotifyBot.java
+++ src/main/java/io/gncloud/coin/server/service/telegram/EventNotifyBot.java
@@ -1,5 +1,7 @@
 package io.gncloud.coin.server.service.telegram;
 
+import io.gncloud.coin.server.model.User;
+import io.gncloud.coin.server.service.IdentityService;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
@@ -15,44 +17,77 @@ public class EventNotifyBot extends TelegramLongPollingBot {
     protected static org.slf4j.Logger logger = LoggerFactory.getLogger(EventNotifyBot.class);
 
     private EventWatchTelegramService service;
+    private IdentityService identityService;
+
     private String username;
     private String token;
 
-    public EventNotifyBot(EventWatchTelegramService service, String username, String token){
+    public EventNotifyBot(EventWatchTelegramService service, IdentityService identityService, String username, String token){
         this.service = service;
+        this.identityService = identityService;
         this.username = username;
         this.token = token;
     }
 
-    public void sendMessage(String chatId, String text) {
+    public void sendMessage(long chatId, String text) {
         SendMessage message = new SendMessage()
                 .setChatId(chatId)
                 .setText(text);
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            logger.error("", e);
+            logger.error(String.format("SendMessage error chatId[%s] text[%s]", chatId, text), e);
         }
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-// We check if the update has a message and the message has text
+        // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                logger.error("", e);
+
+            logger.debug("Receive >> {}", update.getMessage());
+            long chatId = update.getMessage().getChatId();
+            String text = update.getMessage().getText();
+            String userId = service.getUserByTelegram(chatId);
+
+            String responseText = null;
+
+            if(userId == null) {
+                //인증요청
+                if(text.startsWith("/add")) {
+                    String[] cmd = text.split("\\s");
+                    if(cmd.length != 3) {
+                        responseText = "요청이 잘못되었습니다.";
+                    } else {
+                        if(!cmd[0].equals("/add")) {
+                            responseText = "Command 가 잘못되었습니다. /add 로 추정됩니다.";
+                        } else {
+                            String serviceId = cmd[1];
+                            String servicePassword = cmd[2];
+                            try {
+//                                identityService.login(serviceId, servicePassword);
+                                //추가.
+                                responseText = "인증성공. 이제 메시지를 받을 수 있습니다. 해제는 언제든 /quit 명령을 입력해주세요.";
+                                service.setUserChatId(serviceId, chatId);
+                            } catch (Throwable t) {
+                                responseText = "인증이 실패했습니다. 아이디와 비밀번호를 확인해주세요.";
+                            }
+                        }
+                    }
+                } else {
+                    responseText = "사용에 앞서 인증이 필요합니다.\n사용법: /add [사용자아이디] [암호]";
+                }
+            } else {
+                //환영메시지
+                if(text.equals("/quit")) {
+                    service.unsetUserChatId(chatId);
+                    responseText = "계정연결을 성공적으로 취소했습니다.";
+                } else {
+                    responseText = "알 수 없는 명령입니다.";
+                }
             }
 
-            //TODO 추가해달라고 하면 인증후 추가해준다.
-
-            String userId = "";
-            String chatId = "";
-            service.setUserChatId(userId, chatId);
+            sendMessage(chatId, responseText);
         }
     }
 
--- src/main/java/io/gncloud/coin/server/service/telegram/EventWatchTelegramService.java
+++ src/main/java/io/gncloud/coin/server/service/telegram/EventWatchTelegramService.java
@@ -1,5 +1,6 @@
 package io.gncloud.coin.server.service.telegram;
 
+import io.gncloud.coin.server.service.IdentityService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +27,7 @@ public class EventWatchTelegramService {
     protected static org.slf4j.Logger logger = LoggerFactory.getLogger(EventWatchTelegramService.class);
 
     private EventNotifyBot bot;
+    private IdentityService identityService;
 
     @Value("${notify.telegram.botName}")
     private String botName;
@@ -33,34 +35,49 @@ public class EventWatchTelegramService {
     @Value("${notify.telegram.botToken}")
     private String botToken;
 
-    private Map<String, String> userChatIdMap;
+    private Map<String, Long> userChatIdMap;
+    private Map<Long, String> chatIdUserMap;
 
     @PostConstruct
     public void init(){
-        logger.info("Construct TelegramBot. {}, {}", botName, botToken);
+        logger.info("Initialize TelegramBot.. {}, {}", botName, botToken);
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
         try {
-            bot = new EventNotifyBot(this, botName, botToken);
+            bot = new EventNotifyBot(this, identityService, botName, botToken);
             botsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             logger.error("", e);
         }
 
         userChatIdMap = new HashMap<>();
-        //tODO 초기에 어디선가 로딩한다..
+        chatIdUserMap = new HashMap<>();
+        //tODO 초기에 어디선가 로딩한다..  userId, chatId
     }
 
-    public void setUserChatId(String userId, String chatId) {
+    public void setUserChatId(String userId, Long chatId) {
+        // 메모리 저장.
         userChatIdMap.put(userId, chatId);
-        //TODO 어딘가에 저장한다..
-    }
-    public String getChatIdByUserId(String userId) {
-        return userChatIdMap.get(userId);
-    }
-
-    public void sendMessage(String chatId, String text){
-        bot.sendMessage(chatId, text);
+        chatIdUserMap.put(chatId, userId);
+        // 영구저장소 저장.
+        //TODO 어딘가에 저장한다..  userId, chatId
     }
 
+    public void sendMessage(String userId, String text){
+        Long chatId = userChatIdMap.get(userId);
+        if(chatId != null) {
+            bot.sendMessage(chatId, text);
+        }
+    }
+
+    public String getUserByTelegram(long chatId) {
+        return chatIdUserMap.get(chatId);
+    }
+
+    public void unsetUserChatId(long chatId) {
+        String userId = chatIdUserMap.get(chatId);
+
+
+        //TODO unset userId, chatId
+    }
 }
