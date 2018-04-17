dca579ab662ca0f08481deca7083706a044dd241
swsong <swsong@gncloud.kr>
2018-04-17 15:44:00 +0900

    start shell

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -9,7 +9,6 @@ if [ ! -z "$pid" ] ; then
     sleep 2
 fi
 # prevent killed by Hup, ctrl-c
-
 export APP_HOME=/home/ec2-user/coincloud
 
 cd $APP_HOME
@@ -18,6 +17,6 @@ trap '' 1
 JVM_OPT="-Xms4g -Xmx4g -Duser.language=ko -XX:+HeapDumpOnOutOfMemoryError -XX:+UseParallelOldGC -d64 -server"
 SPRING_OPT="--spring.config.location=file:$APP_HOME/conf/application.yaml --logging.pattern.file=$APP_HOME/conf/logback-spring.xml "
 
-$APP_HOME/jdk/bin/java $JVM_OPT -jar $APP_HOME/coincloud-server-$VERSION.jar $SPRING_OPT > $MGB_HOME/output.log 2>&1 &
+$APP_HOME/jdk/bin/java $JVM_OPT -jar $APP_HOME/coincloud-server-$VERSION.jar $SPRING_OPT > $APP_HOME/output.log 2>&1 &
 sleep 1
-tail -f logs/system.log
+tail -f $APP_HOME/logs/system.log
