d834f7a47ef120be7c65d9f2601b92fb4cdacaf9
joonwoo88 <jwkim@gncloud.kr>
2018-07-09 09:32:18 +0900

    .

--- src/main/java/io/systom/coin/api/InvestGoodsController.java
+++ src/main/java/io/systom/coin/api/InvestGoodsController.java
@@ -77,4 +77,5 @@ public class InvestGoodsController extends AbstractController{
         }
     }
 
+
 }
---
+++ src/main/java/io/systom/coin/api/SummaryPerformance.java
@@ -0,0 +1,20 @@
+package io.systom.coin.api;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * create joonwoo 2018. 7. 9.
+ * 
+ */
+@RestController
+@RequestMapping("/performance")
+public class SummaryPerformance {
+
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(SummaryPerformance.class);
+
+
+
+
+}
