d80e5e40d6ad7788acd83bc4eb5b87dac9b59533
joonwoo88 <jwkim@gncloud.kr>
2018-08-03 14:32:51 +0900

    .

---
+++ production/bin/start_live.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+VERSION=1.2.14
+
+pid=`ps -ef|grep java|grep coincloud-server|grep -v grep|awk '{print $2}'`
+if [ ! -z "$pid" ] ; then
+    echo "kill $pid"
+    kill $pid || kill -9 $pid
+    sleep 2
+fi
+sleep 1
+# prevent killed by Hup, ctrl-c
+APP_HOME=/home/ec2-user/systom
+
+cd $APP_HOME
+
+trap '' 1
+JVM_OPT="-Xms4g -Xmx4g -Duser.language=ko -XX:+HeapDumpOnOutOfMemoryError -XX:+UseParallelOldGC -d64 -server"
+SPRING_OPT="--spring.config.location=file:$APP_HOME/conf/application.yml --logging.pattern.file=$APP_HOME/conf/logback-spring.xml "
+
+isLiveExecution=true java $JVM_OPT -jar $APP_HOME/coincloud-server-$VERSION.jar $SPRING_OPT > $APP_HOME/output.log 2>&1 &
+sleep 1
+tail -f $APP_HOME/logs/system.log
--- production/bin/start_mock.sh
+++ production/bin/start_mock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.0.0
+VERSION=1.2.12
 
 pid=`ps -ef|grep java|grep coincloud-server|grep -v grep|awk '{print $2}'`
 if [ ! -z "$pid" ] ; then
@@ -8,8 +8,9 @@ if [ ! -z "$pid" ] ; then
     kill $pid || kill -9 $pid
     sleep 2
 fi
+sleep 1
 # prevent killed by Hup, ctrl-c
-export APP_HOME=/home/ec2-user/coincloud
+APP_HOME=/home/ec2-user/systom
 
 cd $APP_HOME
 
@@ -17,6 +18,6 @@ trap '' 1
 JVM_OPT="-Xms4g -Xmx4g -Duser.language=ko -XX:+HeapDumpOnOutOfMemoryError -XX:+UseParallelOldGC -d64 -server"
 SPRING_OPT="--spring.config.location=file:$APP_HOME/conf/application.yml --logging.pattern.file=$APP_HOME/conf/logback-spring.xml "
 
-ava $JVM_OPT -jar $APP_HOME/coincloud-server-$VERSION.jar $SPRING_OPT > $APP_HOME/output.log 2>&1 &
+java $JVM_OPT -jar $APP_HOME/coincloud-server-$VERSION.jar $SPRING_OPT > $APP_HOME/output.log 2>&1 &
 sleep 1
 tail -f $APP_HOME/logs/system.log
