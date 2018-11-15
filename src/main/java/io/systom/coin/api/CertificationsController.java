e59fca2cf7a30d8b802e302cb1f535409c01b15c
joonwoo88 <jwkim@gncloud.kr>
2018-11-15 18:20:06 +0900

    .

--- src/main/java/io/systom/coin/api/CertificationsController.java
+++ src/main/java/io/systom/coin/api/CertificationsController.java
@@ -7,10 +7,8 @@ import io.systom.coin.service.CertificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.reflect.annotation.ExceptionProxy;
 
 @RestController
 @RequestMapping("/v1/certifications")
